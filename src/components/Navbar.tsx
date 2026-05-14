708e3a5e2290e737d40f25a46b67b42935cb6b9b
mayank3012 <Mayank.nice9411@gmail.com>
2026-05-14 21:58:22 +0530

    change the logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Link, NavLink, useLocation } from "react-router-dom";
-import logo from "@/assets/logo.png";
+import logo from "@/assets/logo.jpg";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
